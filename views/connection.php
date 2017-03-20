092cd5907263dca8664072c77b0ad3d4c7fa3cf6
alexkandres <alexkandres@live.com>
2017-03-20 15:51:01 -0400

    User can log in

---
+++ views/connection.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 3/20/2017
+ * Time: 11:54 AM
+ */
+
+function db_connect(){
+
+//    static $connection;
+
+    if(!isset($connection)){
+
+        $config = parse_ini_file("../config.ini");
+
+        // Create connection
+        $connection = mysqli_connect("localhost", $config["username"], $config["password"], $config["dbname"]);
+
+    }
+
+    // Check connection
+    if ($connection === false) {
+        echo "Connection error";
+        return mysqli_connect_error();
+    }
+
+    echo "Connected successfully";
+    echo "<br>";
+    return $connection;
+
+}
+
+function db_query($query) {
+    // Connect to the database
+    $connection = db_connect();
+
+    // Query the database
+    $result = mysqli_query($connection,$query);
+
+    if($result == false)
+        echo mysqli_error($connection);
+    return $result;
+}
+
+function db_error(){
+    $connection = db_connect();
+    return mysqli_error($connection);
+}
--- views/login_action.php
+++ views/login_action.php
@@ -1,7 +1,78 @@
 <?php
+session_start();
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
+
 /**
  * Created by PhpStorm.
  * User: alex
  * Date: 3/20/2017
  * Time: 9:37 AM
  */
+
+//connects to database
+include "connection.php";
+
+$email = $_GET["email"];
+$password = $_GET["password"];
+
+//select U.sid from user U where email = "$email" AND password='$pass'
+$result = db_query("SELECT U.sid FROM `user` U WHERE `email` = '$email' AND `password` = '$password' ");
+if($result == false){
+    echo "somethings wrong";
+}
+else{
+
+    if(!($row = mysqli_fetch_row($result)))
+        echo "no user found or incorrect credentials";
+
+    //save sid in session and variable
+    $sid = $row[0];
+    $_SESSION["sid"] = $row[0];
+
+    $result->close();
+
+    if(isSuperAdmin($sid)){
+        echo "User is SuperAdmin";
+    }
+    else if(isAdmin($sid)){
+        echo "User is Admin";
+    }
+    else{
+        echo "User is Student only";
+    }
+
+    //    while ($row = $result->fetch_object()){
+//        $user_arr[] = $row;
+//    }
+//    // Free result set
+//    $result->close();
+//    $db->next_result();
+
+}
+
+function isSuperAdmin($sid){
+    $result = db_query("SELECT S.sid FROM superadmin S WHERE S.sid = '$sid' ");
+    if($result == false){
+        echo "Error w/ query";
+    }
+    //check if there is a row
+    else if ($row = mysqli_fetch_row($result)){
+        return true;
+    }
+
+    $result ->close();
+}
+
+function isAdmin($sid){
+    $result = db_query("SELECT A.sid FROM admin A WHERE A.sid = '$sid' ");
+    if($result == false){
+        echo "Error w/ query";
+    }
+    //check if there is a row
+    else if ($row = mysqli_fetch_row($result)){
+        return true;
+    }
+
+    $result->close();
+}
