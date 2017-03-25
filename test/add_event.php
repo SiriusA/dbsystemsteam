2ccf9a1f097b042c667d00d0b35ab63011d7a345
Kyle Picinich <picinich.kyle@knights.ucf.edu>
2017-03-25 14:48:56 -0400

    Fixes to event add

--- test/add_event.php
+++ test/add_event.php
@@ -20,7 +20,7 @@ echo("connection successful");
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 	Event Name: <input type="text" name="eventname"><br>
 	Format:YYYY-MM-DD HH:MM:SS<br>
-	Start Time: <input type="text" name="starttime"> End Time: <input type="text" name="endtime"><br>
+	Start Time: <input type="datetime-local" name="starttime"> End Time: <input type="datetime-local" name="endtime"><br>
 	Description: <textarea name="description" rows="10" cols="40"></textarea><br>
 	Phone: <input type="text" name="phone"> Email: <input type="text" name="email"><br>
 	<input type="submit" name="Submit"><br>
@@ -33,40 +33,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$eventname = test_input($_POST["eventname"]);
 	$starttime = test_input($_POST["starttime"]);
 	$endtime = test_input($_POST["endtime"]);
+	$location = "1";
 	$desc = test_input($_POST["description"]);
 	$phone = test_input($_POST["phone"]);
 	$email = test_input($_POST["email"]);
 	
 	echo("testing insert");
 
-	$timetable_collision = "SELECT e.lid
-	FROM Events_temp e
-	WHERE ( EXISTS (
-		SELECT e2.lid
-		FROM Events_Hosted_Located e2
-		WHERE e.lid = e2.lid
-		AND (
-		(e2.start_time <= e.start_time AND e.start_time < e2.end_time)
-		OR (e2.start_time <= e.end_time AND e.end_time < e2.end_time)
-		OR (e.start_time <= e2.start_time AND e2.start_time < e.end_time)
-		OR (e.start_time <= e2.end_time AND e2.end_time < e.end_time)
-		)
-		)
+	$timetable_collision = "SELECT e2.lid, e2.start_time
+	FROM Events_Hosted_Located e2
+	WHERE " . $location . " = e2.lid
+	AND (
+	(e2.start_time <= '" . $starttime . "' AND '" . $starttime . "' < e2.end_time)
+	OR (e2.start_time <= '" . $endtime . "' AND '" . $endtime . "' < e2.end_time)
+	OR ('" . $starttime . "' <= e2.start_time AND e2.start_time < '" . $endtime . "')
+	OR ('" . $starttime . "' <= e2.end_time AND e2.end_time < '" . $endtime . "')
 	);";
+	echo($timetable_collision);
 	$insert_def_str = "(start_time, end_time, rid, lid, approved, type, visibility, ename, phone, email, description) ";
 	$insert_data_str = "VALUES ('" . $starttime . "', '" . $endtime . "', 1, 1, 1, 1, 1, '" . $eventname . "', '" . $phone . "', '" . $email . "', '" . "1" . "');";
 	echo $insert_data_str;
-	$insert_temp = "INSERT INTO Events_Temp" . $insert_def_str . " " . $insert_data_str;
-	$delete_temp = "DELETE FROM Events_Temp WHERE start_time = '" . $starttime . "' AND lid = '" . "1" . "';";
-	echo $delete_temp;
 	$insert_actual = "INSERT INTO Events_Hosted_Located" . $insert_def_str . " " . $insert_data_str;
 	$conn->query("USE event;");
-	$conn->query($delete_temp);
-	if($conn->query($insert_temp)=== TRUE) {
-		echo "new record created <br>";
-	} else {
-		echo "Error: " . $insert_temp . "<br>" . $conn->error;
-	}
+	
 	$result = $conn->query($timetable_collision);
 	echo("<br> ye <br>");
 	if($result->num_rows == 0) {
@@ -79,7 +68,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	} else {
 		echo "Collision Detected";
 	}
-	$conn->query($delete_temp);
 }
 
 function test_input($data) {
@@ -90,52 +78,6 @@ function test_input($data) {
 }
 
 ?>
-
-<!-- Kept for reference
-<?php
-echo("testing insert");
-$timetable_collision = "SELECT e.lid
-FROM Events_temp e
-WHERE ( EXISTS (
-	SELECT e2.lid
-	FROM Events_Hosted_Located e2
-	WHERE e.lid = e2.lid
-    AND (
-	(e2.start_time <= e.start_time AND e.start_time < e2.end_time)
-	OR (e2.start_time <= e.end_time AND e.end_time < e2.end_time)
-	OR (e.start_time <= e2.start_time AND e2.start_time < e.end_time)
-	OR (e.start_time <= e2.end_time AND e2.end_time < e.end_time)
-	)
-	)
-);";
-$insert_temp = "INSERT INTO Events_Temp(start_time, end_time, rid, lid, description, approved, type, visibility, ename, phone, email)
-VALUES ('2017-03-02 12:10:00', '2017-03-02 12:40:00', 1, 1, 3, 1, 1, 1, 'cirno', '999-999-9999', 'the_strongest@gensokyo.gov');";
-$delete_temp = "DELETE FROM Events_Temp;";
-$insert_actual = "INSERT INTO Events_Hosted_Located(start_time, end_time, rid, lid, description, approved, type, visibility, ename, phone, email)
-VALUES ('2017-03-02 12:10:00', '2017-03-02 12:50:00', 1, 1, 3, 1, 1, 1, 'cirno', '999-999-9999', 'the_strongest@gensokyo.gov');";
-$conn->query("USE event;");
-$conn->query($delete_temp);
-if($conn->query($insert_temp)=== TRUE) {
-	echo "new record created <br>";
-} else {
-    echo "Error: " . $insert_temp . "<br>" . $conn->error;
-}
-$result = $conn->query($timetable_collision);
-echo("<br> ye <br>");
-if($result->num_rows == 0) {
-	if($conn->query($insert_actual)=== TRUE) {
-		echo "new record created <br>";
-		echo("insert successful");
-	} else {
-		echo "Error: " . $insert_actual . "<br>" . $conn->error;
-	}	
-} else {
-	echo "Collision Detected";
-}
-$conn->query($delete_temp);
-
-?>
--->
 
 </body>
 </html>
