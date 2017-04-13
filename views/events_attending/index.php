fa12e2f34ae8429cec93e11b09ecbce5b3909e87
alexkandres <alexkandres@live.com>
2017-04-13 09:45:30 -0400

    Add calendar view

--- views/events_attending/index.php
+++ views/events_attending/index.php
@@ -34,151 +34,133 @@
 
 ?>
 
-<div id="content" class="container-fluid">
-    <h2>Events Attending</h2>
+    <div id="content" class="container-fluid">
+        <h2>Events Attending</h2>
 
-        <!--        1/1 row-->
-        <div class="row">
+            <!--        1/1 row-->
+            <div class="row">
 
-            <!--                    1/2 column-->
-            <!--            place buttons here-->
-            <div class="col-sm-6" style="background-color: #00bfbf">
+                <!--                    1/2 column-->
+                <!--            place buttons here-->
+                <div class="col-sm-6">
 
-                <div class="list-group">
-                    <div class="row">
-                        <div class="col-sm-5">
-                            <a href="#" class="list-group-item">Event Description</a>
+                    <div class="list-group">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="#" class="list-group-item">Event Description</a>
+                            </div>
+                            <div class="col-sm-2">
+                                <a href="#" class="list-group-item">Date</a>
+                            </div>
+                            <div class="col-sm-2">
+                                <a href="#" class="list-group-item">University</a>
+                            </div>
+                            <div class="col-sm-2">
+                                <a href="#" class="list-group-item">Status</a>
+                            </div>
                         </div>
-                        <div class="col-sm-2">
-                            <a href="#" class="list-group-item">Date</a>
-                        </div>
-                        <div class="col-sm-2">
-                            <a href="#" class="list-group-item">University</a>
-                        </div>
-                        <div class="col-sm-2">
-                            <a href="#" class="list-group-item">Status</a>
-                        </div>
+
                     </div>
 
+                    <?php
+                    if(empty($_GET["page"])){
+                        $page = 0;
+                        $remaining = sizeof($eventAttendingList);
+                    }
+                    else{
+                        $page = $_GET["page"];
+                        $remaining = $_GET["remaining"];
+                    }
+
+    //                echo $eventAttendingList[0]["ename"] . "<br>";
+    //                echo $eventAttendingList[0]["description"] . "<br>";
+    //                echo $eventAttendingList[0]["start_time"] . "<br>";
+    //                echo $eventAttendingList[0]["end_time"] . "<br>";
+    //                echo $eventAttendingList[0]["approved"] . "<br>";
+
+                    $nextPage = -1;
+                    //        only display 10
+                    echo '<ul class="list-group">';
+                    if($remaining > 10){
+                        $index = sizeof($eventAttendingList) - $remaining;
+                        for($i = 0; $i < 10; $i++){
+                            echo '<div class="row">
+                                    <div class="col-sm-5">
+                                        <a href="../rso_description/index.php?index='.($i + $index).'" class="list-group-item">'.$eventAttendingList[$i + $index]["ename"].' - '.$eventAttendingList[$i]["description"].'</a>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["start_time"].'</a>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <a href="#" class="list-group-item">University</a>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["approved"].'</a>
+                                    </div>
+                                  </div>';
+                        }
+                        $nextPage = $page + 1;
+                    }
+                    //        display what is left
+                    else{
+                        $index = sizeof($eventAttendingList) - $remaining;
+                        for($i = $index; $i < sizeof($eventAttendingList); $i++){
+                            echo '<div class="row">
+                                    <div class="col-sm-5">
+                                        <a href="../rso_description/index.php?index='.($i + $index).'" class="list-group-item">'.$eventAttendingList[$i + $index]["ename"].' - '.$eventAttendingList[$i]["description"].'</a>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["start_time"].'</a>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <a href="#" class="list-group-item">University</a>
+                                    </div>
+                                    <div class="col-sm-1">
+                                        <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["approved"].'</a>
+                                    </div>
+                                  </div>';
+                        }
+                    }
+                    echo '</ul>';
+
+                    //start pager
+                    echo '<div class="row">';
+                        echo    '<ul class="pager">';
+
+                            //display previous button?
+                            if($page != 0){
+                            $prevPage = $page - 1;
+                            $prevRemaining = $remaining + 10;
+                            //DISPLAY PREV BUTTON
+                            echo    '<li><a href="index.php?page='.$prevPage.'&remaining='.$prevRemaining.'">Previous</a></li>';
+
+                            }
+                            echo    '<li>Page '.$page.'</li>';
+
+                            //display next button?
+                            if($nextPage != -1){
+                            $nextRemaining = $remaining - 10;
+                            //display next button here
+                            echo '<li><a href="index.php?page='.$nextPage.'&remaining='.$nextRemaining.'">Next</a></li>';
+
+                            }
+                            echo    '</ul>';
+                        echo '</div>';
+                    //end pager
+                    ?>
                 </div>
 
-                <?php
-                if(empty($_GET["page"])){
-                    $page = 0;
-                    $remaining = sizeof($eventAttendingList);
-                }
-                else{
-                    $page = $_GET["page"];
-                    $remaining = $_GET["remaining"];
-                }
 
-//                echo $eventAttendingList[0]["ename"] . "<br>";
-//                echo $eventAttendingList[0]["description"] . "<br>";
-//                echo $eventAttendingList[0]["start_time"] . "<br>";
-//                echo $eventAttendingList[0]["end_time"] . "<br>";
-//                echo $eventAttendingList[0]["approved"] . "<br>";
-
-                $nextPage = -1;
-                //        only display 10
-                echo '<ul class="list-group">';
-                if($remaining > 10){
-                    $index = sizeof($eventAttendingList) - $remaining;
-                    for($i = 0; $i < 10; $i++){
-                        echo '<div class="row">
-                                <div class="col-sm-5">
-                                    <a href="../rso_description/index.php?index='.($i + $index).'" class="list-group-item">'.$eventAttendingList[$i + $index]["ename"].' - '.$eventAttendingList[$i]["description"].'</a>
-                                </div>
-                                <div class="col-sm-4">
-                                    <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["start_time"].'</a>
-                                </div>
-                                <div class="col-sm-2">
-                                    <a href="#" class="list-group-item">University</a>
-                                </div>
-                                <div class="col-sm-2">
-                                    <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["approved"].'</a>
-                                </div>
-                              </div>';
-                    }
-                    $nextPage = $page + 1;
-                }
-                //        display what is left
-                else{
-                    $index = sizeof($eventAttendingList) - $remaining;
-                    for($i = $index; $i < sizeof($eventAttendingList); $i++){
-                        echo '<div class="row">
-                                <div class="col-sm-5">
-                                    <a href="../rso_description/index.php?index='.($i + $index).'" class="list-group-item">'.$eventAttendingList[$i + $index]["ename"].' - '.$eventAttendingList[$i]["description"].'</a>
-                                </div>
-                                <div class="col-sm-4">
-                                    <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["start_time"].'</a>
-                                </div>
-                                <div class="col-sm-2">
-                                    <a href="#" class="list-group-item">University</a>
-                                </div>
-                                <div class="col-sm-1">
-                                    <a href="#" class="list-group-item">'.$eventAttendingList[$i + $index]["approved"].'</a>
-                                </div>
-                              </div>';
-                    }
-                }
-                echo '</ul>';
-
-                //start pager
-                echo '<div class="row">';
-                    echo    '<ul class="pager">';
-
-                        //display previous button?
-                        if($page != 0){
-                        $prevPage = $page - 1;
-                        $prevRemaining = $remaining + 10;
-                        //DISPLAY PREV BUTTON
-                        echo    '<li><a href="index.php?page='.$prevPage.'&remaining='.$prevRemaining.'">Previous</a></li>';
-
-                        }
-                        echo    '<li>Page '.$page.'</li>';
-
-                        //display next button?
-                        if($nextPage != -1){
-                        $nextRemaining = $remaining - 10;
-                        //display next button here
-                        echo '<li><a href="index.php?page='.$nextPage.'&remaining='.$nextRemaining.'">Next</a></li>';
-
-                        }
-                        echo    '</ul>';
-                    echo '</div>';
-                //end pager
-                ?>
+                <!--                2/2 column-->
+                <!--            Calendar here-->
+                <div class="col-sm-6">
+                        <iframe src="https://calendar.google.com/calendar/embed?src=8c1o31v12mspk56gncmoqb6a70%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="700" height="550" frameborder="0" scrolling="no"></iframe>
+                </div>
             </div>
 
 
-            <!--                2/2 column-->
-            <!--            Map here-->
-            <div class="col-sm-6" style="background-color: #00fafa">
-                <div id="googleMap" style="width:100%;height:400px;"></div>
-            </div>
-        </div>
 
-
-
-</div>
+    </div>
 
 
 </body>
-<!--    $i = 0;-->
-<!---->
-<!--    if(sizeof($eventAttendingList) <= 0){-->
-<!--      echo '<div class="list-group-item">';-->
-<!--      echo '<h3> Looks like your not going anywhere! </h3>';-->
-<!--      echo '</div>';-->
-<!--    }-->
-<!--    else-->
-<!--      while($i < 20 && $i < sizeof($event_result)){-->
-<!--        echo $eventAttendingList[$i++]["ename"] . "<br>";-->
-<!--        echo $eventAttendingList[$i++]["description"] . "<br>";-->
-<!--        echo $eventAttendingList[$i++]["start_time"] . "<br>";-->
-<!--        echo $eventAttendingList[$i++]["end_time"] . "<br>";-->
-<!--        echo $eventAttendingList[$i++]["approved"] . "<br>";-->
-<!--      }-->
-<!--?>-->
-<!---->
-<!--</body>-->
