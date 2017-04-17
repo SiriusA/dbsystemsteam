3d66728183be7dd7ebffdd5620e8f9c60a8307a5
alexkandres <alexkandres@live.com>
2017-04-16 23:50:54 -0400

    Display image of university

--- views/list_rso_superadmin/index.php
+++ views/list_rso_superadmin/index.php
@@ -1,8 +1,8 @@
 <!-- Prevent user from skipping login page -->
 <?php
-  session_start();
+session_start();
 
-  if($_SESSION["userLoggedIn"] == false)
+if($_SESSION["userLoggedIn"] == false)
     header('Location: /');
 ?>
 
@@ -11,7 +11,7 @@
 <head>
     <title>List RSO</title>
 
-    <link rel="stylesheet" href="/stylesheets/create_university.css">
+    <link rel="stylesheet" href="/stylesheets/list_rso.css">
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -26,36 +26,102 @@
 
 <body>
 
+<!--    TODO place correct navbar depending on user-->
 <!--    navbar start-->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">Events</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="../create_university">Create University</a></li>
-                <li><a href="../list_university">List University</a></li>
-                <li class="active"><a href="#">List RSO</a></li>
-                <li><a href="../list_events">List Events</a></li>
-                <li><a href = "/db/logout.php">Log Out</a></li>
-            </ul>
-        </div>
-    </nav>
-<!--    navbar end-->
+<?php
 
+if($_SESSION["usertype"] == 1)
+    include "../nav_bar/super_admin_navbar.php";
+else{
+
+}
+?>
+<!--navbar end-->
+
+<div class="container-fluid">
+    <h2>List RSO</h2>
     <?php
 
     include "../db/query_rso.php";
+    //TODO query correct RSO to display for each user (Super, Admin, Student)
+    //query RSO from Universities that ae managed by SuperAdmin
     $rsoAndUniverstiyInfo = getRSOsFromManagedUniversities();
 
-echo '<div id="content" class="container-fluid">
-        <h2>Create University</h2>
-        '.$rsoAndUniverstiyInfo[0]["rname"].'<br>
-        '.$rsoAndUniverstiyInfo[0]["uname"].'<br>
-        '.$rsoAndUniverstiyInfo[0]["approved"].'<br>
-      </div>';
+    //if page is not set, then default to page 0
+    if(empty($_GET["page"])){
+        $page = 0;
+        $remaining = sizeof($rsoAndUniverstiyInfo);
+    }
+    else{
+        $page = $_GET["page"];
+        $remaining = $_GET["remaining"];
+    }
+
+    $nextPage = -1;
+    //        only display 10
+    echo '<ul class="list-group">';
+    if($remaining > 10){
+        $index = sizeof($rsoAndUniverstiyInfo) - $remaining;
+        for($i = 0; $i < 10; $i++){
+            echo     '<div class="row">
+                        <div class="col-sm-8">
+                            <a href="../rso_description/index.php?index='.($i + $index).'" class="list-group-item">'.$rsoAndUniverstiyInfo[$i + $index]["rname"].' - '.$rsoAndUniverstiyInfo[$i]["description"].'</a>
+                        </div>
+                        <div class="col-sm-1">
+                            <a href="#" class="list-group-item">'.$rsoAndUniverstiyInfo[$i + $index]["uname"].'</a>
+                        </div>
+                        <div class="col-sm-1">
+                            <a href="#" class="list-group-item">'.$rsoAndUniverstiyInfo[$i + $index]["approved"].'</a>
+                        </div>
+                      </div>';
+        }
+        $nextPage = $page + 1;
+    }
+//        display what is left
+    else{
+        $index = sizeof($rsoAndUniverstiyInfo) - $remaining;
+        for($i = $index; $i < sizeof($rsoAndUniverstiyInfo); $i++){
+            echo     '<div class="row">
+                        <div class="col-sm-8">
+                            <a href="../rso_description/index.php?index='.$i.'" class="list-group-item">'.$rsoAndUniverstiyInfo[$i]["rname"].' - '.$rsoAndUniverstiyInfo[$i]["description"].'</a>
+                        </div>
+                        <div class="col-sm-1">
+                            <a href="#" class="list-group-item">'.$rsoAndUniverstiyInfo[$i]["uname"].'</a>
+                        </div>
+                        <div class="col-sm-1">
+                            <a href="#" class="list-group-item">'.$rsoAndUniverstiyInfo[$i]["approved"].'</a>
+                        </div>
+                      </div>';
+        }
+    }
+    echo '</ul>';
+
+    //start pager
+    echo '<div class="row">';
+    echo    '<ul class="pager">';
+
+    //display previous button?
+    if($page != 0){
+        $prevPage = $page - 1;
+        $prevRemaining = $remaining + 10;
+        //DISPLAY PREV BUTTON
+        echo    '<li><a href="index.php?page='.$prevPage.'&remaining='.$prevRemaining.'">Previous</a></li>';
+
+    }
+    echo    '<li>Page '.$page.'</li>';
+
+    //display next button?
+    if($nextPage != -1){
+        $nextRemaining = $remaining - 10;
+        //display next button here
+        echo '<li><a href="index.php?page='.$nextPage.'&remaining='.$nextRemaining.'">Next</a></li>';
+
+    }
+    echo    '</ul>';
+    echo '</div>';
+    //end pager
 
     ?>
+</div>
 </body>
 </html>
