4cffba3c090b6160add11a04b43f5adf5fafd483
RaRaMe <r.ram2014@knights.ucf.edu>
2017-04-17 21:29:16 -0400

    comment setup

---
+++ views/db/query_comments.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "connection.php";
+
+function getEventComments(){
+
+    $result = db_query("SELECT C.comment, C.timestamp, U.first_name, U.last_name
+                        FROM comment C, events E, user U
+                        WHERE C.start_time = E.e_start AND C.lid = E.lid");
+
+    if($result == false){
+        echo "something went wrong";
+    }
+
+        //save each row in array
+    $eventComments = array();
+    while($row = mysqli_fetch_array($result)){
+        $eventComments[] = $row;
+    }
+
+    return $eventComments;
+}
